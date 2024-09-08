f3eade989ffddf0c6863085538b4f785cb4d736d
janisfs <janisfs@mail.ru>
2024-09-08 22:29:43 +0300

    solve some mistakes

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def my_html_page():
+def my_main_page():
     return render_template("index.html")
+
+@app.route("/blog/")
+def my_blog_page():
+    return render_template("blog.html")
+
+@app.route("/contacts/")
+def my_contacts_page():
+    return render_template("contacts.html")
 
 if __name__ == "__main__":
     app.run()
